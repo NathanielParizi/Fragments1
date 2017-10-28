2860c3d8628dc35af0cd35f735cec2af095a9ca7
Nathaniel-Joel Parizi <forexmagnumopus@hushmail.com>
2017-10-27 23:52:43 -0400

    Initial commit

---
+++ app/src/main/java/com/jtanuki/go/fragments1/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jtanuki.go.fragments1.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jtanuki.go.fragments1.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
